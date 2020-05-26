97f0444546953b4014222527538c6d97acd02673
Dan Hogan <danhogan78@gmail.com>
2020-05-26 17:29:38 -0400

    setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-data-table
+      :headers="headers"
+      :items="allData"
+      :sort-by="['calories', 'fat']"
+      :sort-desc="[false, true]"
+      multi-sort
+      class="elevation-1"
+    ></v-data-table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import allData from '../../data.json'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      headers: [
+        {
+          align: "start",
+          sortable: false,
+          value: "name"
+        },
+        { text: "IG Handle", value: "ig_handle" },
+        { text: "Squat", value: "squat" },
+        { text: "Bench", value: "bench" },
+        { text: "Deadlift", value: "deadlift" },
+        { text: "Total", value: "total" }
+      ],
+      allData: allData
+    };
+  },
+  created(){
+      console.log(this.desserts)
   }
-}
+};
 </script>
